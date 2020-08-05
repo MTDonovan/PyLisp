434d1e43f4369b2353e38a0b013227c00e6487c5
MTD <MTDonovan@outlook.com>
2020-08-04 19:59:18 -0600

    2020-08-04 19:59:18Z

--- script.py
+++ script.py
@@ -1,28 +1,31 @@
 from inspect import signature as sig
-from functools import partial as par
+from functools import partial
+
+
+# if len(sig(func).parameters) > 1:
+#     value = func(value)
+# else:
+#     value = func(value)
 
 def pipe(value, *args, **kwargs):
-    for func in list(args):
-        # if len(sig(func).parameters) > 1:
-        #     value = func(value)
-        # else:
-        #     value = func(value)
-        value = func(value)
-
+    for expr in list(args):
+        if isinstance(expr, tuple) or isinstance(expr, list):
+            value = partial(*expr)(value)
+        else:
+            value = expr(value)
     return value
 
-def add(arg, value):
-    return arg + value
+def add(*args):
+    from functools import reduce
+    return reduce(lambda x, y: x + y, list(args))
 
-# (pipe
-#  (100,
-#   (par (add, 10)),
-#   (print)))
+(pipe
+ (100,
+  (add, 10, 20, 30),
+  (print)))
 
 (print
  (pipe
   (100,
-   (par (add, 10)),
-   (lambda x: x / 2)
-  )
- ))
+   (add, 10),
+   (lambda x: x / 2))))
