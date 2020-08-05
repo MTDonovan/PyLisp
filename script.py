1e24ace07be8094b6065673e574a5dc4ea99b0d3
MTD <MTDonovan@outlook.com>
2020-08-04 20:57:42 -0600

    2020-08-04 20:57:41Z

--- script.py
+++ script.py
@@ -10,7 +10,8 @@ from functools import partial
 def pipe(value, *args, **kwargs):
     for expr in list(args):
         if isinstance(expr, tuple) or isinstance(expr, list):
-            value = partial(*expr)(value)
+            # value = partial(value)(*expr)
+            value = partial(expr[0], value)(*expr[1:])
         else:
             value = expr(value)
     return value
@@ -29,12 +30,3 @@ def add(*args):
   (100,
    (add, 10),
    (lambda x: x / 2))))
-
-var = (pipe
-       (1,
-        (add, 10, 20, 30),
-        (add, 10, 20, 30)))
-
-(pipe
- (var,
-  (print)))
