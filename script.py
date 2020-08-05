2da934344fbf2a058c0ab8c01ef7f938ac8c5634
MTD <MTDonovan@outlook.com>
2020-08-04 22:56:14 -0600

    psg

--- script.py
+++ script.py
@@ -1,31 +1,26 @@
 from inspect import signature as sig
-from functools import partial
+from functools import partial, reduce
 
-
-# if len(sig(func).parameters) > 1:
-#     value = func(value)
-# else:
-#     value = func(value)
 
 def pipe(value, *args, **kwargs):
     for expr in list(args):
         if isinstance(expr, tuple) or isinstance(expr, list):
-            value = partial(expr[0], value)(*expr[1:])
+            if expr[0] in ("+", "-", "*", "/"):
+                value = partial(reduce, lambda x, y: eval("{0} {1} {2}".format(x, expr[0], y)))([value, *expr[1:]])
+            else:
+                value = partial(expr[0], value)(*expr[1:])
         else:
             value = expr(value)
     return value
 
-def add(*args):
-    from functools import reduce
-    return reduce(lambda x, y: x + y, list(args))
 
 (pipe
  (100,
-  (add, 10, 20, 30),
+  ('+', 2, 2, 2),
   (print)))
 
 (print
  (pipe
   (100,
-   (add, 10),
+   ('+', 10),
    (lambda x: x / 2))))
