f64040f6f45ad4956ddff5cf32b558fc91e0c9e1
Matthew Thomas Donovan <MTDonovan@outlook.com>
2020-08-10 14:30:19 -0600

    Update script.py

--- script.py
+++ script.py
@@ -24,28 +24,24 @@ def pipe(value, *args, **kwargs):
             value = expr(value)
     return value
 
-
 (pipe
  ('TEST',
   (lambda x: x.lower()),
-  (print)
- ))
-
+  (print)))
 
 # (pipe
 #  (100,
 #   ('+', 2, 2, 2),
-#   (print)
-#  ))
+#   (print)))
+#
 # (print
 #  (pipe
 #   (100,
 #    ('+', 10),
-#    (lambda x: x / 2.25))
-#  ))
+#    (lambda x: x / 2.25))))
+#
 # (pipe
 #  (100,
 #   ('/', 2),
 #   ('+', 100),
-#   (print)
-#  ))
+#   (print)))
